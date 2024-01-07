8bc738c075d431f6d9373398bd541ca9b7dc1c28
Faisal <faisal.hassan@tracxn.com>
2024-01-08 00:52:16 +0530

    restructured

--- src/main/java/git/Git.java
+++ src/main/java/git/Git.java
@@ -52,8 +52,8 @@ public class Git {
             Paths.get(".git")
     );
 
-    private static final byte[] SPACE_BYTES = { ' ' };
-    private static final byte[] NULL_BYTES = { 0 };
+    private static final byte[] SPACE_BYTES = {' '};
+    private static final byte[] NULL_BYTES = {0};
 
     private final Path root;
 
@@ -77,137 +77,60 @@ public class Git {
         return getDotGit().resolve("config");
     }
 
-    public Blob readBlob(String hash) throws FileNotFoundException, IOException {
-        return readObject(ObjectType.BLOB, hash);
-    }
+    //    ---------------------------------ALL INIT RELATED CODE IS BELOW---------------------------------
+    public static Git init(Path root) throws IOException {
+        final var git = new Git(root);
 
-    public Commit readCommit(String hash) throws FileNotFoundException, IOException {
-        return readObject(ObjectType.COMMIT, hash);
-    }
-
-    public Tree readTree(String hash) throws FileNotFoundException, IOException {
-        return readObject(ObjectType.TREE, hash);
-    }
-
-    public <T extends GitObject> T readObject(ObjectType<T> type, String hash) throws FileNotFoundException, IOException {
-        final var first2 = hash.substring(0, 2);
-        final var remaining38 = hash.substring(2);
-
-        final var path = getObjectsDirectory().resolve(first2).resolve(remaining38);
-
-        try (
-                final var inputStream = new FileInputStream(path.toFile());
-                final var inflaterInputStream = new InflaterInputStream(inputStream)
-        ) {
-            final var builder = new StringBuilder();
-
-            int value;
-            while ((value = inflaterInputStream.read()) != -1 && value != ' ') {
-                builder.append((char) value);
-            }
-
-            final var typeString = builder.toString();
-            if (!type.getName().equals(typeString)) {
-                throw new IllegalArgumentException("trying to read %s as %s (%s)".formatted(typeString, type.getName(), hash));
-            }
-
-            builder.setLength(0);
-            while ((value = inflaterInputStream.read()) != -1 && value != 0) {
-                builder.append((char) value);
-            }
-
-            @SuppressWarnings("unused")
-            final var length = Integer.parseInt(builder.toString());
-
-            return type.deserialize(new DataInputStream(inflaterInputStream));
-        }
-    }
-
-    public RawObject readRawObject(String hash) throws FileNotFoundException, IOException {
-        final var first2 = hash.substring(0, 2);
-        final var remaining38 = hash.substring(2);
-
-        final var path = getObjectsDirectory().resolve(first2).resolve(remaining38);
-
-        try (
-                final var inputStream = new FileInputStream(path.toFile());
-                final var inflaterInputStream = new InflaterInputStream(inputStream)
-        ) {
-            final var builder = new StringBuilder();
-
-            int value;
-            while ((value = inflaterInputStream.read()) != -1 && value != ' ') {
-                builder.append((char) value);
-            }
-
-            final var type = ObjectType.byName(builder.toString());
-
-            builder.setLength(0);
-            while ((value = inflaterInputStream.read()) != -1 && value != 0) {
-                builder.append((char) value);
-            }
-
-            final var length = Integer.parseInt(builder.toString());
-            final var content = inflaterInputStream.readNBytes(length);
-
-            return new RawObject(type, content);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public String writeObject(git.domain.GitObject object) throws IOException, NoSuchAlgorithmException {
-        final var objectType = ObjectType.byClass(object.getClass());
-
-        return writeRawObject(objectType.serialize(object));
-    }
-
-    public String writeRawObject(RawObject object) throws IOException, NoSuchAlgorithmException {
-        final var content = object.content();
-        final var lengthBytes = String.valueOf(content.length).getBytes();
-
-        byte[] data;
-        try (
-                final var outputStream = new ByteArrayOutputStream();
-                final var dataOutputStream = new DataOutputStream(outputStream)
-        ) {
-            outputStream.write(object.type().getName().getBytes());
-            outputStream.write(SPACE_BYTES);
-            outputStream.write(lengthBytes);
-            outputStream.write(NULL_BYTES);
-            outputStream.write(content);
-
-            data = outputStream.toByteArray();
+        final var dotGit = git.getDotGit();
+        if (Files.exists(dotGit)) {
+            throw new FileAlreadyExistsException(dotGit.toString());
         }
 
-        return writeRawObject(data);
+        Files.createDirectories(git.getObjectsDirectory());
+        Files.createDirectories(git.getRefsDirectory());
+
+        final var head = git.getHeadFile();
+        Files.createFile(head);
+        Files.write(head, "ref: refs/heads/master\n".getBytes());
+
+        final var config = git.getConfigFile();
+        Files.createFile(config);
+        Files.write(config, ("[core]\n        autocrlf = false").getBytes());
+
+        return git;
     }
 
-    public String writeRawObject(byte[] data) throws IOException, NoSuchAlgorithmException {
-        final var hashBytes = MessageDigest.getInstance("SHA-1").digest(data);
-        final var hash = HexFormat.of().formatHex(hashBytes);
+//    ---------------------------------ALL OPEN RELATED CODE IS BELOW---------------------------------
 
-        final var first2 = hash.substring(0, 2);
-        final var first2Directory = getObjectsDirectory().resolve(first2);
-        Files.createDirectories(first2Directory);
+    public static Git open(Path root) throws IOException {
+        final var git = new Git(root);
 
-        final var remaining38 = hash.substring(2);
-        final var path = first2Directory.resolve(remaining38);
-
-        try (
-                final var outputStream = Files.newOutputStream(path);
-                final var deflaterOutputStream = new DeflaterOutputStream(outputStream);
-        ) {
-            deflaterOutputStream.write(data);
+        final var dotGit = git.getDotGit();
+        if (!Files.exists(dotGit)) {
+            throw new NoSuchFileException(dotGit.toString());
         }
 
-        return hash;
+        return git;
     }
+
+//    ---------------------------------ALL BLOB RELATED CODE IS BELOW---------------------------------
 
     public String writeBlob(Path path) throws IOException, NoSuchAlgorithmException {
         final var bytes = Files.readAllBytes(path);
         final var blob = new Blob(bytes);
 
         return writeObject(blob);
+    }
+
+    public Blob readBlob(String hash) throws FileNotFoundException, IOException {
+        return readObject(ObjectType.BLOB, hash);
+    }
+
+
+//    ---------------------------------ALL TREE RELATED CODE IS BELOW---------------------------------
+
+    public Tree readTree(String hash) throws FileNotFoundException, IOException {
+        return readObject(ObjectType.TREE, hash);
     }
 
     public String writeTree(Path root) throws IOException, NoSuchAlgorithmException {
@@ -249,6 +172,13 @@ public class Git {
         return writeObject(tree);
     }
 
+
+//    ---------------------------------ALL COMMIT RELATED CODE IS BELOW---------------------------------
+
+    public Commit readCommit(String hash) throws FileNotFoundException, IOException {
+        return readObject(ObjectType.COMMIT, hash);
+    }
+
     public String writeCommit(String treeHash, String parentHash, AuthorSignature author, String message) throws NoSuchAlgorithmException, IOException {
         return writeObject(new Commit(
                 treeHash,
@@ -259,77 +189,73 @@ public class Git {
         ));
     }
 
-    public void checkout(Tree tree) throws FileNotFoundException, IOException {
-        checkout(tree, root);
-    }
+//    ---------------------------------OBJECT READ RELATED CODE IS BELOW---------------------------------
 
-    public void checkout(Tree tree, Path root) throws FileNotFoundException, IOException {
-        for (final var entry : tree.entries()) {
-            switch (entry.mode().type()) {
-                case REGULAR_FILE: {
-                    final var blob = readBlob(entry.hash());
-                    final var path = root.resolve(entry.name());
+    public <T extends GitObject> T readObject(ObjectType<T> type, String hash) throws FileNotFoundException, IOException {
+        final var first2 = hash.substring(0, 2);
+        final var remaining38 = hash.substring(2);
 
-                    checkout(blob, path);
+        final var path = getObjectsDirectory().resolve(first2).resolve(remaining38);
 
-                    break;
-                }
+        try (
+                final var inputStream = new FileInputStream(path.toFile());
+                final var inflaterInputStream = new InflaterInputStream(inputStream)
+        ) {
+            final var builder = new StringBuilder();
 
-                case DIRECTORY: {
-                    final var subTree = readTree(entry.hash());
-                    final var subRoot = root.resolve(entry.name());
-
-                    Files.createDirectories(subRoot);
-                    checkout(subTree, subRoot);
-
-                    break;
-                }
-
-                default: {
-                    throw new UnsupportedOperationException("entry type: " + entry.mode().type());
-                }
+            int value;
+            while ((value = inflaterInputStream.read()) != -1 && value != ' ') {
+                builder.append((char) value);
             }
+
+            final var typeString = builder.toString();
+            if (!type.getName().equals(typeString)) {
+                throw new IllegalArgumentException("trying to read %s as %s (%s)".formatted(typeString, type.getName(), hash));
+            }
+
+            builder.setLength(0);
+            while ((value = inflaterInputStream.read()) != -1 && value != 0) {
+                builder.append((char) value);
+            }
+
+            @SuppressWarnings("unused") final var length = Integer.parseInt(builder.toString());
+
+            return type.deserialize(new DataInputStream(inflaterInputStream));
         }
     }
 
-    public void checkout(Blob blob, Path path) throws FileNotFoundException, IOException {
-        System.err.println("checkout %s".formatted(path));
+//    ---------------------------------OBJECT WRITE RELATED CODE IS BELOW---------------------------------
 
-        Files.write(path, blob.data());
+    @SuppressWarnings("unchecked")
+    public String writeObject(git.domain.GitObject object) throws IOException, NoSuchAlgorithmException {
+        final var objectType = ObjectType.byClass(object.getClass());
+
+        return writeRawObject(objectType.serialize(object));
     }
 
-    public static Git init(Path root) throws IOException {
-        final var git = new Git(root);
+    public String writeRawObject(byte[] data) throws IOException, NoSuchAlgorithmException {
+        final var hashBytes = MessageDigest.getInstance("SHA-1").digest(data);
+        final var hash = HexFormat.of().formatHex(hashBytes);
 
-        final var dotGit = git.getDotGit();
-        if (Files.exists(dotGit)) {
-            throw new FileAlreadyExistsException(dotGit.toString());
+        final var first2 = hash.substring(0, 2);
+        final var first2Directory = getObjectsDirectory().resolve(first2);
+        Files.createDirectories(first2Directory);
+
+        final var remaining38 = hash.substring(2);
+        final var path = first2Directory.resolve(remaining38);
+
+        try (
+                final var outputStream = Files.newOutputStream(path);
+                final var deflaterOutputStream = new DeflaterOutputStream(outputStream);
+        ) {
+            deflaterOutputStream.write(data);
         }
 
-        Files.createDirectories(git.getObjectsDirectory());
-        Files.createDirectories(git.getRefsDirectory());
-
-        final var head = git.getHeadFile();
-        Files.createFile(head);
-        Files.write(head, "ref: refs/heads/master\n".getBytes());
-
-        final var config = git.getConfigFile();
-        Files.createFile(config);
-        Files.write(config, ("[core]\n        autocrlf = false").getBytes());
-
-        return git;
+        return hash;
     }
 
-    public static Git open(Path root) throws IOException {
-        final var git = new Git(root);
 
-        final var dotGit = git.getDotGit();
-        if (!Files.exists(dotGit)) {
-            throw new NoSuchFileException(dotGit.toString());
-        }
-
-        return git;
-    }
+//    ---------------------------------ALL CLONE RELATED CODE IS BELOW---------------------------------
 
     public static Git clone(URI uri, Path path) throws IOException, DataFormatException, NoSuchAlgorithmException {
         final var client = new GitClient(uri);
@@ -392,10 +318,102 @@ public class Git {
         return git;
     }
 
+    public RawObject readRawObject(String hash) throws FileNotFoundException, IOException {
+        final var first2 = hash.substring(0, 2);
+        final var remaining38 = hash.substring(2);
+
+        final var path = getObjectsDirectory().resolve(first2).resolve(remaining38);
+
+        try (
+                final var inputStream = new FileInputStream(path.toFile());
+                final var inflaterInputStream = new InflaterInputStream(inputStream)
+        ) {
+            final var builder = new StringBuilder();
+
+            int value;
+            while ((value = inflaterInputStream.read()) != -1 && value != ' ') {
+                builder.append((char) value);
+            }
+
+            final var type = ObjectType.byName(builder.toString());
+
+            builder.setLength(0);
+            while ((value = inflaterInputStream.read()) != -1 && value != 0) {
+                builder.append((char) value);
+            }
+
+            final var length = Integer.parseInt(builder.toString());
+            final var content = inflaterInputStream.readNBytes(length);
+
+            return new RawObject(type, content);
+        }
+    }
+
+    public String writeRawObject(RawObject object) throws IOException, NoSuchAlgorithmException {
+        final var content = object.content();
+        final var lengthBytes = String.valueOf(content.length).getBytes();
+
+        byte[] data;
+        try (
+                final var outputStream = new ByteArrayOutputStream();
+                final var dataOutputStream = new DataOutputStream(outputStream)
+        ) {
+            outputStream.write(object.type().getName().getBytes());
+            outputStream.write(SPACE_BYTES);
+            outputStream.write(lengthBytes);
+            outputStream.write(NULL_BYTES);
+            outputStream.write(content);
+
+            data = outputStream.toByteArray();
+        }
+
+        return writeRawObject(data);
+    }
+
+    public void checkout(Tree tree) throws FileNotFoundException, IOException {
+        checkout(tree, root);
+    }
+
+    public void checkout(Tree tree, Path root) throws FileNotFoundException, IOException {
+        for (final var entry : tree.entries()) {
+            switch (entry.mode().type()) {
+                case REGULAR_FILE: {
+                    final var blob = readBlob(entry.hash());
+                    final var path = root.resolve(entry.name());
+
+                    checkout(blob, path);
+
+                    break;
+                }
+
+                case DIRECTORY: {
+                    final var subTree = readTree(entry.hash());
+                    final var subRoot = root.resolve(entry.name());
+
+                    Files.createDirectories(subRoot);
+                    checkout(subTree, subRoot);
+
+                    break;
+                }
+
+                default: {
+                    throw new UnsupportedOperationException("entry type: " + entry.mode().type());
+                }
+            }
+        }
+    }
+
+    public void checkout(Blob blob, Path path) throws FileNotFoundException, IOException {
+        System.err.println("checkout %s".formatted(path));
+
+        Files.write(path, blob.data());
+    }
+
     @SuppressWarnings("rawtypes")
     private static record RawObject(
             ObjectType type,
             byte[] content
-    ) {}
+    ) {
+    }
 
 }
